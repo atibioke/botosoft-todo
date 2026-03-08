2e397192d758fa3917331ba8f9818579dc60ae5e
Rasaq Ganiyu <eudoxusganium@gmail.com>
2026-03-08 21:07:43 +0100

    Updating TodoFilter.tsx and rewriting drag and drop logic for proper update

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,27 +1,27 @@
 import '../styles/ConfirmModal.css'
 
 type Props = {
-  isOpen:  boolean
-  onConfirm: () =>  void
-  onClose: () =>  void
+  isOpen: boolean
+  onConfirm: () => void
+  onClose: () => void
 }
 export default function ConfirmModal({ isOpen, onClose, onConfirm }: Props) {
   if (!isOpen) return null
 
   return (
-    <div className="modal-overlay" >
-      <div className="modal" >
-        <h3 className='delete-modal-header'>Delete Todo</h3 >
-        <p className='delete-modal-subheader'>Are you sure you want to delete this todo?</p >
+    <div className="modal-overlay">
+      <div className="modal">
+        <h3 className='delete-modal-header'>Delete Todo</h3>
+        <p className='delete-modal-subheader'>Are you sure you want to delete this todo?</p>
 
-        <div className="modal-buttons" >
-          <button className="cancel-btn" onClick={onClose} >
+        <div className="modal-buttons">
+          <button className="cancel-btn" onClick={onClose}>
             Cancel
-          </button >
+          </button>
 
-          <button className="delete-btn" onClick={onConfirm} >
+          <button className="delete-btn" onClick={onConfirm}>
             Delete
-          </button >
+          </button>
         </div >
       </div >
     </div >
